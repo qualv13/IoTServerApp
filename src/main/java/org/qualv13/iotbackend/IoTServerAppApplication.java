2ed24a7e41e0d31bc1cb14fc5f763f2656642b1c
qualv13 <71326761+qualv13@users.noreply.github.com>
2025-11-22 18:24:40 +0100

    init

---
+++ src/main/java/org/qualv13/iotbackend/IoTServerAppApplication.java
@@ -0,0 +1,13 @@
+package org.qualv13.iotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTServerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/qualv13/iotbackend/IoTServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qualv13.iotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
